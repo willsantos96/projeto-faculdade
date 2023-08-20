55a3eca4c7d15620eadedb5345dc873bf4c63c66
willsantos96 <willsantos96@gmail.com>
2023-08-20 05:03:06 -0300

    add rota home

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views  # Importe as funções de visualização da sua aplicação
 
 urlpatterns = [
+    path('', views.home, name='home'),  # URL para a página home
     path('adicionar/', views.adicionar, name='adicionar'),  # URL para a página de adicionar aluno
     path('formulario/', views.formulario, name='formulario'),    # URL para a página de validação do aluno
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
+def home (request):
+    # Lógica para renderizar a página home
+    return render(request, 'home.html')
+
 def adicionar (request):
     # Lógica para renderizar a página do formulário
     return render(request, 'adicionar.html')
